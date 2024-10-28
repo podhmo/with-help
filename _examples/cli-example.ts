0f22c039a02cc6c9b40b26b756d6513728ad8b2f
podhmo <ababjam61+github@gmail.com>
2024-10-28 22:44:30 +0900

    chore: add example (broken)

--- _examples/cli-example.ts
+++ _examples/cli-example.ts
@@ -1,19 +1,25 @@
-import { parseArgs } from "node:util";
+// import { parseArgs } from "jsr:podhmo/with-help"
+import { buildHelp, parseArgs } from "../mod.ts";
 
-const { values, positionals } = parseArgs({
-    options: {
-        "no-color": { type: 'boolean', default: false },
-        version: { type: 'string', alias: 'v' }
-    },
-    allowPositionals: true
-});
-const flags = { ...values, color: !values["no-color"] };
+const flagsDefinition = {
+    string: ["version"],
+    boolean: ["color", "help"],
+    negatable: ["color"],
+    default: { color: true },
+
+    required: ["version"],
+}
+
+console.log(buildHelp(flagsDefinition));
+
+const flags = parseArgs(Deno.args, flagsDefinition);
 
 // deno run cli-example.ts --version=1.0.0
 // deno run cli-example.ts --no-color --version=1.0.0
 //
 // hmm
-// deno run cli-example.ts 
+// deno run cli-example.ts
 // deno run cli-example.ts --colr=false --version=1.0.0 x y z
+
 console.dir(flags, { depth: null });
-console.dir(positionals, { depth: null });
+console.dir(flags._, { depth: null });
