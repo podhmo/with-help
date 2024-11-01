5b363cb3c6a63b608b54dcbc90240c6bf5b579ad
podhmo <ababjam61+github@gmail.com>
2024-11-02 06:06:56 +0900

    chore: more tests

--- e2e-tests/tools/gen.ts
+++ e2e-tests/tools/gen.ts
@@ -9,7 +9,7 @@ interface TestCase {
         negatable?: string[]
         collect?: string[]
         required?: string[]
-        default?: Record<string,unknown>
+        default?: Record<string, unknown>
     }
 }
 
@@ -32,7 +32,7 @@ function main() {
 
     // success case
 
-    const testcases: TestCase[] = [
+    const successCases: TestCase[] = [
         { title: "good-00empty", options: {} },
 
         { title: "good-10string1-booleanU-negatableU-collectU-requiredU", options: { string: [["name", "string | undefined"]] } },
@@ -74,16 +74,43 @@ function main() {
         { title: "good-32string2-booleanU-negatableU-collectU-required1", options: { string: [["name", "string"], ["nickname", "string | undefined"]], required: ["name"] } },
         { title: "good-33string2-booleanU-negatableU-collectU-required2", options: { string: [["name", "string"], ["nickname", "string"]], required: ["name", "nickname"] } },
         { title: "good-34string2-booleanU-negatableU-collectU-requiredU-default0", options: { string: [["name", "string | undefined"], ["nickname", "string | undefined"]], default: {} } },
-        { title: "good-35string2-booleanU-negatableU-collectU-requiredU-default1", options: { string: [["name", "string"], ["nickname", "string | undefined"]], default: {name: "FOO"} } },
-        { title: "good-36string2-booleanU-negatableU-collectU-requiredU-default1U", options: { string: [["name", "string | undefined"], ["nickname", "string | undefined"]], default: {name: undefined} } },
+        { title: "good-35string2-booleanU-negatableU-collectU-requiredU-default1", options: { string: [["name", "string"], ["nickname", "string | undefined"]], default: { name: "FOO" } } },
+        { title: "good-36string2-booleanU-negatableU-collectU-requiredU-default1U", options: { string: [["name", "string | undefined"], ["nickname", "string | undefined"]], default: { name: undefined } } },
+
+        { title: "good-40string2-boolean1-negatableU-collectU-required1", options: { string: [["name", "string"], ["nickname", "string | undefined"]], boolean: [["color", "boolean"]], required: ["name"] } },
     ]
 
-    // TODO: failure case
-    // - unexpected option in required (string, boolean, another)
-    // - unexpected option in collect (string, boolean, another)
-    // - unexpected option in string (string, boolean, another)
+    // failure case
+    const failureCases: TestCase[] = [
+        { title: "bad-00-stringU-booleanU--unexpected-negatable", options: { negatable: ["XXX"] } },
+        { title: "bad-01-stringU-booleanU--unexpected-collect", options: { collect: ["XXX"] } },
+        { title: "bad-02-stringU-booleanU--unexpected-required", options: { required: ["XXX"] } },
+        { title: "bad-03-stringU-booleanU--unexpected-default", options: { default: { XXX: "FOO" } } }, // 許容する？
+        { title: "bad-04-string0-booleanU--unexpected-negatable", options: { string: [], negatable: ["XXX"] } },
+        { title: "bad-05-string0-booleanU--unexpected-collect", options: { string: [], collect: ["XXX"] } },
+        { title: "bad-06-string0-booleanU--unexpected-required", options: { string: [], required: ["XXX"] } },
+        { title: "bad-07-string0-booleanU--unexpected-default", options: { string: [], default: { XXX: "FOO" } } }, // 許容する？
 
-    for (const tc of testcases) {
+        { title: "bad-10-string1-booleanU--unexpected-negatable", options: { string: [["name", "string | undefined"]], negatable: ["XXX"] } },
+        { title: "bad-11-string1-booleanU--unexpected-collect", options: { string: [["name", "string | undefined"]], collect: ["XXX"] } },
+        { title: "bad-12-string1-booleanU--unexpected-required", options: { string: [["name", "string | undefined"]], required: ["XXX"] } },
+        { title: "bad-13-string1-booleanU--unexpected-default", options: { string: [["name", "string | undefined"]], default: { XXX: "FOO" } } }, // 許容する？
+
+        { title: "bad-20-stringU-boolean1--unexpected-negatable", options: { boolean: [["color", "boolean"]], negatable: ["XXX"] } },
+        { title: "bad-21-stringU-boolean1--unexpected-collect", options: { boolean: [["color", "boolean"]], collect: ["XXX"] } },
+        { title: "bad-22-stringU-boolean1--unexpected-required", options: { boolean: [["color", "boolean"]], required: ["XXX"] } },
+        { title: "bad-23-stringU-boolean1--unexpected-default", options: { boolean: [["color", "boolean"]], default: { XXX: "FOO" } } }, // 許容する？
+
+        { title: "bad-30-string1-boolean1--unexpected-negatable", options: { string: [["name", "string | undefined"]], boolean: [["color", "boolean"]], negatable: ["name"] } },
+        { title: "bad-31-string1-boolean1--unexpected-collect", options: { string: [["name", "string | undefined"]], boolean: [["color", "boolean"]], collect: ["color"] } },
+        { title: "bad-32-string1-boolean1--unexpected-required", options: { string: [["name", "string | undefined"]], boolean: [["color", "boolean"]], required: ["xxxx"] } },
+        { title: "bad-33-string1-boolean1--unexpected-default", options: { string: [["name", "string | undefined"]], boolean: [["color", "boolean"]], default: { XXX: "FOO" } } }, // 許容する？
+
+        // conflict?
+        // { title: "bad-40-string1-boolean1--conflict", options: { string: [["name", "string | undefined"]], boolean: [["name", "boolean"]] } },
+    ]
+
+    for (const tc of successCases.concat(failureCases)) {
         const code = [];
         code.push(`import { parseArgs } from "../parse-args.ts"`);
         code.push(`const parsed = parseArgs(Deno.args,  {`);
