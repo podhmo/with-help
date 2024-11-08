0d817494bfc94ab6384f31a6c8f7c799873b561a
podhmo <ababjam61+github@gmail.com>
2024-11-09 02:02:05 +0900

    choices example (#25)
    * chore: add examples (broken)
    
    * chore: update help text
    
    * chore: this is a work-around
    
    * chore: main()

---
+++ examples/use-choices.ts
@@ -0,0 +1,35 @@
+import { parseArgs } from "../src/parse-args.ts";
+import { buildHelp } from "../src/build-help.ts";
+
+const directions = ["north", "south", "east", "west"] as const;
+type DirectionType = typeof directions[number];
+
+const options = {
+  string: ["name", "direction"],
+  boolean: ["color"],
+  negatable: ["color"],
+  required: ["name", "direction"],
+  default: { name: "world" },
+  flagDescription: {
+    direction: `(required) choose one of: ${JSON.stringify(directions)}`,
+  },
+} as const;
+
+function main() {
+  // args is of type { name: string, direction: string }
+  const args = parseArgs(Deno.args, options);
+  if (!directions.includes(args.direction as DirectionType)) {
+    console.log(buildHelp(options));
+    console.error(`Invalid direction: "${args.direction}" is not one of ${JSON.stringify(directions)}`);
+    Deno.exit(1);
+  }
+
+  // args2 is of type { name: string, direction: DirectionType }
+  const args2 = { ...args, direction: args.direction as DirectionType };
+
+  console.dir(args2, { depth: null });
+}
+
+if (import.meta.main) {
+  main();
+}
